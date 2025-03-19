8744927d29ceb594649f7dd9b327aaeba110c182
Nicorb <67009524+nicola-corbellini@users.noreply.github.com>
2025-03-19 23:59:50 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ echo Activating virtual environment...
 call .touchsam\Scripts\activate
 echo Installing dependencies
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install ultralytics
+pip install ultralytics==8.3.93
 pip install numpy<2
 echo Installation complete.
 
