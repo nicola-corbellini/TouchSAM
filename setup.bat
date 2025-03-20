d6066a81aba1a1b48b57ee0cb41455486779aa41
Nicorb <67009524+nicola-corbellini@users.noreply.github.com>
2025-03-20 10:17:41 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,9 +6,12 @@ echo Creating .venv directory...
 %PYTHON_PATH% -m venv ".touchsam"
 echo Activating virtual environment...
 call .touchsam\Scripts\activate
-echo Installing dependencies
+echo Installing dependencies...
+echo Installing Torch
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+echo Installing Ultralytics
 pip install ultralytics==8.3.93
+echo Downgrading numpy
 pip install numpy<2
 echo Installation complete.
 
